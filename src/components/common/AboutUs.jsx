b9b037bcc290336c3aa8e54d1d9283bcd9089e53
Meet Vasani <w.vasanimeet@gmail.com>
2024-02-16 15:15:51 +0530

    about us

--- src/components/common/AboutUs.jsx
+++ src/components/common/AboutUs.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const AboutUs = () => {
   return (
-    <div>Bridging Healthcare Gaps Through Flexible Staffing
-
-    MedConnect is India's first end-to-end platform focused on connecting quality freelance doctors with hospitals facing resource constraints and talent shortages. We are on a mission to bridge the rural-urban healthcare access divide by efficient matching of medical talent to locations that need it the most.
-    
-    Our integrated portal allows hospitals to easily post part-time or temporary doctor positions across departments, while verified doctors can sign up to indicate their availability and interest areas. Our proprietary algorithms then map the best assignments based on credentials, fees, preferences and requirements.
-    
-    By enabling flexible job options for doctors and better capacity utilization, MedConnect helps enhance healthcare infrastructure and quality across the country. We incentivize doctor participation in rural postings through our award programs. With seamless profile management, appointment tracking and built-in payments, we are streamlining healthcare freelancing and placement processes using technology.</div>
-  )
+    <div style={{ maxWidth: '600px', margin: 'auto', padding: '20px' }}>
+      <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Bridging Healthcare Gaps Through Flexible Staffing</h2>
+      <p>
+        Focused on connecting quality freelance doctors with hospitals facing resource constraints and talent shortages. We are on a mission to bridge the rural-urban healthcare access divide by efficiently matching medical talent to locations that need it the most.
+      </p>
+      <p>
+        Our integrated portal allows hospitals to easily post part-time or temporary doctor positions across departments, while verified doctors can sign up to indicate their availability and interest areas. Our proprietary algorithms then map the best assignments based on credentials, fees, preferences, and requirements.
+      </p>
+      <p>
+        By enabling flexible job options for doctors and better capacity utilization, It helps enhance healthcare infrastructure and quality across the country. We incentivize doctor participation in rural postings through our award programs. With seamless profile management, appointment tracking, and built-in payments, we are streamlining healthcare freelancing and placement processes using technology.
+      </p>
+    </div>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
