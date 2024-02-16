b9d097cd663deb4c8c6736c0ecc3dd2f2033f7bd
Adityaweb23 <74668650+Adityaweb23@users.noreply.github.com>
2024-02-16 12:45:02 +0530

    doctor ui started

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Signup from './components/authentication/signup';
 import { ToastContainer } from 'react-toastify';
 import Footer from './components/common/Footer';
 import LoginPage from './components/authentication/Login';
+import Doctor from './components/doctor/Doctor';
+import HospitalJobList from './components/doctor/HospitalJobList';
+import NgoJobList from './components/doctor/NgoJobList';
+
 
 function App() {
   return (
@@ -15,6 +19,9 @@ function App() {
         {/* <Route path="/" element={<Home />} /> */}
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/signup" element={<Signup/>} />
+        <Route path="/hospitalJobList" element={<HospitalJobList/>} />
+        <Route path="/ngoJobList" element={<NgoJobList/>} />
+        <Route path="/doctor" element={<Doctor/>} />
       </Routes>
       <ToastContainer /> 
       <Footer/>
---
+++ src/components/doctor/HospitalJobList.jsx
@@ -0,0 +1,59 @@
+
+import React, { useEffect, useState } from "react";
+import hospitalImage from "../../image/hospital.png"
+import ngoImage from "../../image/NGO.png"
+import { NavLink, useNavigate } from "react-router-dom";
+import JobCard from "./JobCard";
+
+const HospitalJobList = () => {
+    const jobsdata = [
+        {
+        id:1,
+        name: "Bob",
+        price: 10,
+        hospital: "sanjivni ",
+        description:"this is the best job to apply ",
+        required:7
+      },{
+        id:2,
+        name: "Bob",
+        price: 10,
+        hospital: "sanjivni ",
+        description:"this is the best job to apply ",
+        required:7
+      },{
+        id:3,
+        name: "Bob",
+        price: 10,
+        hospital: "sanjivni ",
+        description:"this is the best job to apply ",
+        required:7
+      },{
+        id:4,
+        name: "Bob",
+        price: 10,
+        hospital: "sanjivni ",
+        description:"this is the best job to apply ",
+        required:7
+      },{
+        id:5,
+        name: "Bob",
+        price: 10,
+        hospital: "sanjivni ",
+        description:"this is the best job to apply ",
+        required:7
+      },
+    ];
+
+    console.log(jobsdata);
+  return (
+    <div className="container text-center">
+                    <h1 className='display-3'>Hospital Job List Follows</h1>
+        { jobsdata.map((job,index) => {
+                  return ( <JobCard key={index} name={job.name} price={job.price} hospital={job.hospital} description={job.description} required={job.required}/>);
+        })}
+    </div>
+  );
+};
+
+export default HospitalJobList;
